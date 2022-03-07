23b5b08b32da638f854433d410e348fe7da88250
Noam Levi <lnoam2001"gmail.com>
2022-03-07 23:50:57 +0200

    create sequelize code example

---
+++ examples/sequelize/index.ts
@@ -0,0 +1,26 @@
+import {Column, DataType, Model, Sequelize, Table} from "sequelize-typescript";
+import {getTableInfo} from "../../src/GetTableInfo";
+import {SequelizeCallback} from "../../src/callbacks/SequelizeCallback";
+
+
+@Table({tableName: "Person"})
+class Person extends Model {
+    @Column(DataType.STRING)
+    name: string
+}
+
+
+(async () => {
+    const sequelize = new Sequelize({
+        username: "sierra",
+        password: "sierra",
+        host: "localhost",
+        database: "test",
+        dialect: "postgres"
+    });
+    sequelize.addModels([Person])
+    type PersonPropPath =  { name: string};
+    const person: PersonPropPath = {} as PersonPropPath;
+    const test1 = await getTableInfo(sequelize, "Person", {}, person, SequelizeCallback, "name");
+    console.log(test1.name);
+})();
